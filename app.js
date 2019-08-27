bcb7c40b05ce81354521117ec5990cfa743081b7
mschales <mich.schales@gmail.com>
2019-08-27 23:34:56 +0300

    updates

--- app.js
+++ app.js
@@ -2,17 +2,48 @@ const getNavLinks = document.getElementById("menu").querySelectorAll("a");
 const about = document.getElementById("about");
 const header = document.getElementById("header");
 let curHash = '';
-let offsetValue = 200;
+let offsetValue = 180;
+let scrollOffset = 48;
+let ulSelector = document.querySelector('ul');
 
 setupNavItemsListeners();
 
+
+function setScrollOffset() {
+    if (window.screen.width > 1300) {
+        scrollOffset = 128;
+    }
+
+    else if (window.screen.width < 1300 && window.screen.width > 960) {
+      scrollOffset = 98;      
+    }
+}
+
 function setupNavItemsListeners() {
   getNavLinks.forEach(navItem => {
+
+    navItem.parentElement.addEventListener('click', () => {
+      if (window.screen.width <= 800) {
+        getNavLinks.forEach(navItem => {
+          navItem.parentElement.classList.remove("active");
+        });
+        document.getElementById('home').classList.remove('mobile-menu');
+      }
+    })
+
     navItem.addEventListener("click", e => {
       clearActiveNavItems();
-      e.target.parentElement.classList.add("active");
-    });
-  });
+      let gethash = e.target.hash.substr(1, (e.target.hash.length));
+
+    ulSelector.style.display = 'none';
+    ulSelector.style.visibility = "hidden";
+    ulSelector.classList.remove("active");
+      
+      setScrollOffset();
+      smoothScroll(gethash);
+      e.preventDefault();
+    })
+  })
 }
 
 function lookNavItem() {
@@ -33,6 +64,36 @@ function clearActiveNavItems() {
   });
 }
 
+function smoothScroll (domToScroll) {{
+
+  window.scrollTo({
+     top: document.getElementById(domToScroll).getBoundingClientRect().top + window.pageYOffset - scrollOffset, 
+    behavior: 'smooth' });
+}}
+
 document.addEventListener("scroll", () => {
     lookNavItem();
 });
+
+
+document.querySelector('.mobile-menu-button').addEventListener('click', () => {
+  if (ulSelector.classList.contains("active")) {
+    ulSelector.classList.remove("active");
+    ulSelector.style.display = 'none';
+    ulSelector.style.visibility = "hidden"; 
+  }
+  
+  else {
+    ulSelector.style.visibility = "visible"; 
+    ulSelector.style.display = 'block';
+    ulSelector.classList.add("active");
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.screen.width > 800) {
+    ulSelector.style.visibility = "visible"; 
+    ulSelector.style.display = 'block';
+  }
+});
+
